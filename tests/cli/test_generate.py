26166583c36ffae6da3935057da872f7409bae4c
Daniel Edgecombe <edgy.edgemond@gmail.com>
2024-08-15 18:05:03 +0100

    Add tests for handling failed hooks

--- tests/cli/test_generate.py
+++ tests/cli/test_generate.py
@@ -531,6 +531,24 @@ def test_generate_handles_invalid_hooks(
     assert "Invalid hook" in result.output
 
 
+def hook(_current, _new):
+    return ["test_path"]
+
+
+@pytest.mark.usefixtures("_conventional_commits", "changelog")
+def test_generate_handles_valid_hooks(
+    cli_runner,
+    monkeypatch,
+    config_factory,
+):
+    config_factory(hooks=["tests.cli.test_generate:hook"])
+    monkeypatch.setattr(typer, "confirm", mock.MagicMock(return_value=True))
+    result = cli_runner.invoke(["generate", "--version-tag", "0.3.2", "--no-release"])
+
+    assert result.exit_code == 1
+    assert mock_git.commit.call_args == mock.call("0.0.0", "0.3.2", "v0.3.2", ["CHANGELOG.md", "test_path"])
+
+
 @pytest.mark.usefixtures("_conventional_commits", "changelog")
 def test_generate_uses_supplied_version_part(
     cli_runner,
